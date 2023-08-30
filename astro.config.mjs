c7246b9d07f45af76e54a977e7f815af95da48a3
gzuzmark <alonso.m144@.com>
2023-08-30 12:35:58 -0500

    feat: Add translation components and language pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
+import astroI18next from "astro-i18next";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), svelte()]
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap(),
+    svelte(),
+    astroI18next(),
+  ],
 });
--- src/components/ContactBtn.svelte
+++ src/components/ContactBtn.svelte
@@ -2,6 +2,9 @@
   import { isContactVisible } from "../store/contact";
   import Icon from "./Icon.svelte";
   import Button from "./ui/Button.svelte";
+  import i18next, { t, changeLanguage } from "i18next";
+
+  export let text: string;
 
   function showContactDialog() {
     isContactVisible.set(true);
@@ -12,6 +15,6 @@
   on:click={showContactDialog}
   class="flex gap-1 items-center justify-center"
   block={false}>
-  Contact us now!
+  {text}
   <Icon icon="paper-plane" size="1.2em" />
 </Button>
