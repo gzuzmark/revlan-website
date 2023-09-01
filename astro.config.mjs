a57e43d5fdfee7f812405041ae0b3e9321e375ab
gzuzmark <alonso.m144@.com>
2023-09-01 11:21:25 -0500

    Add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,21 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import astroI18next from "astro-i18next";
-
+import nodejs from "@astrojs/node";
 import svelte from "@astrojs/svelte";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  }),
   experimental: {
-    hybridOutput: true,
+    hybridOutput: true
   },
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    sitemap(),
-    svelte(),
-    astroI18next(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), svelte(), astroI18next()]
 });
