df80656bf0608890587bb9f678c96d973c2ddd0f
gzuzmark <alonso.m144@.com>
2023-09-01 12:45:13 -0500

    Remove hybrid mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,6 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: node({
-    mode: "standalone",
-  }),
   integrations: [
     tailwind(),
     image({
