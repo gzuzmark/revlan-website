1a046196a3f397ba07a5dc908b9381fd75145ff2
gzuzmark <alonso.m144@gmail.com>
2024-04-02 14:39:22 -0500

    Add astro compress to the build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import astroI18next from "astro-i18next";
 import nodejs from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless";
 import svelte from "@astrojs/svelte";
+import compress from 'astro-compress'
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), astroI18next(), svelte(), tailwind()]
+  integrations: [sitemap(), astroI18next(), svelte(), tailwind(), compress()]
 });
